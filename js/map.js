d15f3497eee86202daa7cfe4475ec004973bb542
Razqr07 <71629000+Razqr07@users.noreply.github.com>
2023-06-22 11:38:45 +0200

    FIX AMK

---
+++ js/map.js
@@ -0,0 +1,39 @@
+function togglepopup() {
+    var popup = document.getElementById("popup-1");
+    popup.classList.toggle("active");
+}
+
+function togglepopup2() {
+    var popup = document.getElementById("popup-2");
+    popup.classList.toggle("active");
+}
+
+function togglepopup3() {
+    var popup = document.getElementById("popup-3");
+    popup.classList.toggle("active");
+}
+
+function togglepopup4() {
+    var popup = document.getElementById("popup-4");
+    popup.classList.toggle("active");
+}
+
+function togglepopup5() {
+    var popup = document.getElementById("popup-5");
+    popup.classList.toggle("active");
+}
+
+function togglepopup6() {
+    var popup = document.getElementById("popup-6");
+    popup.classList.toggle("active");
+}
+
+function togglepopup7() {
+    var popup = document.getElementById("popup-7");
+    popup.classList.toggle("active");
+}
+
+function togglepopup8() {
+    var popup = document.getElementById("popup-8");
+    popup.classList.toggle("active");
+}
